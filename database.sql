7f076532437d8bed411bbf5acc201f7370dd0697
v2-kaj <madalitsomuva@gmail.com>
2024-03-23 17:23:05 +0200

    first commit

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE muvaclouddb;
+
+USE muvaclouddb;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ register_user.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User account registration</title>
+    <link rel="stylesheet" href="flexible.css" />
+</head>
+
+<body>
+    <div class='row'>
+        <div class='col-4'></div>
+        <div class='col-4'>
+            <?php
+            session_start();
+            include_once "connectdb.php";
+            $email = $_POST["user_email"];
+            $password = $_POST["user_password"];
+            $firstname= $_POST["firstname"];
+            $lastname= $_POST["lastname"];
+
+            // Check if username already exists
+            $checkStmt = $conn->prepare("SELECT email FROM users WHERE email = ?");
+            $checkStmt->bind_param("s", $username);
+            $checkStmt->execute();
+            $checkResult = $checkStmt->get_result();
+
+            if ($checkResult->num_rows > 0) {
+                $_SESSION['email_taken'] = true;
+                header("Location: create_account.php");
+            } else {
+                // Hash the user's password
+                $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+                // Create a prepared statement
+                $stmt = $conn->prepare("INSERT INTO users (firstname, lastname, email, password) VALUES (?, ?, ?, ?)");
+                $stmt->bind_param("ssss", $firstname, $lastname, $email, $hashedPassword);
+
+                // Execute the statement
+                if ($stmt->execute()) {
+                    // This block will execute if data was successfully inserted into the database   
+                    echo "<p>You have successfully registered</p>";
+                    echo "<a href='login.php'>Go to the login page</a>";
+                } else {
+                    echo "Error: " . $stmt->error;
+                }
+            }
+
+            // Close the statements and connection
+            $checkStmt->close();
+            $stmt->close();
+            $conn->close();
+            ?>
+        </div>
+        <div class='col-4'></div>
+    </div>
+</body>
+
+</html>
